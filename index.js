a63f53c35b78fdc40e8b1866c49a90c15937b304
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2020-06-08 14:05:42 +0300

    response

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ bot.onText(/\/start/, (msg) => {
     });
 });
 
+bot.on('callback_query', query => {
+    if(query.data === "random")
+    {
+        request(
+            config.get('random'), (error, response, body) => {
+
+                const data = JSON.parse(body)[0];
+
+                bot.sendMessage(query.message.chat.id, 
+                    `Name : ${data.name}\nTagline: ${data.tagline}\n First brewed: ${data.first_brewed}\nDescription ${data.description}`
+                    );
+
+                const pic_stream = request.get(data.image_url).on('error', err => console.log(err));
+
+                bot.sendPhoto(query.message.chat.id, pic_stream); 
+            });
+    }
+});
